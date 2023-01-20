843d3502fcfa849f8994342683bccd3773167817
Josh Poduska <josh.poduska@dominodatalab.com>
2023-01-20 11:21:22 -0800

    output to artifacts
    Pushed from Domino: https://demo2.dominodatalab.com/workspace/josh_poduska/COVID-r-markdown?executionId=63cad924ab92a65b16f178a9

--- render.R
+++ render.R
@@ -1,2 +1,2 @@
 rmarkdown::render("covid_mortality_report.Rmd", 
-                  output_file = 'artifacts/covid_mortality_report.html')
+                  output_file = '../artifacts/covid_mortality_report.html')
